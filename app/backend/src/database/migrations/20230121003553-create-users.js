02062ab4cb382af6de10336da910a2527f5e0594
Alex <alexsandrolima991@hotmail.com>
2023-01-20 21:54:17 -0300

    Criação das migrations e renomeação dos aquivos seeders

---
+++ app/backend/src/database/migrations/20230121003553-create-users.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('users');
+  }
+};
---
+++ app/backend/src/database/migrations/20230121003928-create-teams.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('teams', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      team_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'team_name',
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('teams');
+  }
+};
